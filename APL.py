9b38f992ed160b5458fa3ac9d5aa526cdc7d831b
AmIVoid <fukinvoid@gmail.com>
2021-01-01 23:51:50 +1000

    API request to collect all Planning Anime
    API request to write all planning anime into a JSON file.

---
+++ APL.py
@@ -0,0 +1,54 @@
+import requests
+import json
+from ratelimit import limits, sleep_and_retry
+
+RATE_LIMIT = 60
+CALLS = 60
+
+@sleep_and_retry
+@limits(calls=CALLS, period=RATE_LIMIT)
+def search(search):
+    # Here we define our query as a multi-line string
+    query = '''
+  query ($username: String, $type: MediaType, $status: MediaListStatus) {
+    MediaListCollection(userName: $username, type: $type, status: $status) {
+        lists {
+            name
+            entries {
+                status
+                media {
+                    title { romaji }
+                    format
+                    id
+                    averageScore
+              }
+            }
+          }
+        }
+      }
+  '''
+
+    # Define our query variables and values that will be used in the query request
+    variables = {
+        'username': search,
+        'type': 'ANIME',
+        'status': 'PLANNING'
+    }
+
+    url = 'https://graphql.anilist.co'
+
+    # Make the HTTP Api request
+    response = requests.post(url, json={'query': query, 'variables': variables})
+
+    text_out = response.text
+
+    with open('planning.json', 'w') as out_file:
+        out_file.write(text_out)
+
+
+def getVars():
+  return input("Anilist username: ")
+
+
+if __name__ == '__main__':
+    search(getVars())
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # APL
  Automatic Anime watch priority list maker with AniList and Google Sheets
+
+# Currently WIP
+
+## Purpose
+This program was made so I could more easily rank which anime I should watch first as I have a planning list of over 250+. This would make my previous method of ranking anime to watch first take significantly less time.
+
+## APL Score v1
+The APL Score is based on 3 factors:
+1. Mean Score (M) - Base score of Anime.
+2. B Factor (B) - Bingability of a series over 12 episodes long but less than 50 episodes long with a score over 75%.
+3. P Factor (P) - If the previous season has been watched.
+
+APL Score = M * (1 + (B+P))
+
