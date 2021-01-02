3d7a61bf0cbd32a8ca7551d05e2a8fb756e0d0d3
AmIVoid <fukinvoid@gmail.com>
2021-01-02 21:40:10 +1000

    Anime filtering and beautification
    Filter out all Anime that is not TV format or Finished airing.
    + Beatifying files

--- APL.py
+++ APL.py
@@ -1,60 +1,53 @@
 import requests
-import json
 from ratelimit import limits, sleep_and_retry
 from parseJSON import parseFunc
 
 RATE_LIMIT = 60
 CALLS = 60
 
+
 @sleep_and_retry
 @limits(calls=CALLS, period=RATE_LIMIT)
 def search(search):
-    # Here we define our query as a multi-line string
-    query = '''
-  query ($username: String, $type: MediaType, $status: MediaListStatus) {
-    MediaListCollection(userName: $username, type: $type, status: $status) {
-        lists {
-            name
-            entries {
-                status
-                media {
-                    title { romaji }
-                    episodes
-                    duration
-                    averageScore
-                    format
-                    status
-                    id
-              }
-            }
-          }
-        }
-      }
-  '''
+    query = """
+	query ($username: String, $type: MediaType, $status: MediaListStatus) {
+		MediaListCollection(userName: $username, type: $type, status: $status) {
+				lists {
+						name
+						entries {
+								status
+								media {
+										title { romaji }
+										episodes
+										duration
+										averageScore
+										format
+										status
+										id
+							}
+						}
+					}
+				}
+			}
+	"""
 
-    # Define our query variables and values that will be used in the query request
-    variables = {
-        'username': search,
-        'type': 'ANIME',
-        'status': 'PLANNING'
-    }
+    variables = {"username": search, "type": "ANIME", "status": "PLANNING"}
 
-    url = 'https://graphql.anilist.co'
+    url = "https://graphql.anilist.co"
 
-    # Make the HTTP Api request
-    response = requests.post(url, json={'query': query, 'variables': variables})
+    response = requests.post(url, json={"query": query, "variables": variables})
 
     text_out = response.text
 
-    with open('planning.json', 'w') as out_file:
+    with open("planning.json", "w") as out_file:
         out_file.write(text_out)
 
 
 def getVars():
-  return input("Anilist username: ")
+    return input("Anilist username: ")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     search(getVars())
 
 parseFunc()
--- parseJSON.py
+++ parseJSON.py
@@ -1,15 +1,23 @@
 import json
+import os
+
 
 def parseFunc():
-    with open('planning.json') as json_data:
+    with open("planning.json") as json_data:
         data = json.load(json_data)
 
     parsed = data["data"]["MediaListCollection"]["lists"][0]["entries"]
 
-    for element in parsed: 
-        del element['status']
+    for element in parsed:
+        del element["status"]
 
-    objects = json.dumps(parsed, indent=4)
+    parse_format = [x for x in parsed if x["media"]["format"] == "TV"]
 
-    with open('parsed.json', 'w') as out_file:
-            out_file.write(objects)
+    parse_status = [x for x in parse_format if x["media"]["status"] == "FINISHED"]
+
+    objects = json.dumps(parse_status, indent=4)
+
+    with open("parsed.json", "w") as out_file:
+        out_file.write(objects)
+
+    os.remove("planning.json")
