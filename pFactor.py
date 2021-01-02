ec443597565c14036419da236cd3be5b3f7a212d
AmIVoid <fukinvoid@gmail.com>
2021-01-03 02:18:49 +1000

    Added planning filter for use with relations function
    Added planning filter to grab all anime ID's from planning list

--- pFactor.py
+++ pFactor.py
@@ -5,7 +5,8 @@ import os
 
 RATE_LIMIT = 60
 CALLS = 60
-listId = 97767
+listId = 20652
+
 
 @sleep_and_retry
 @limits(calls=CALLS, period=RATE_LIMIT)
@@ -31,37 +32,58 @@ def relations():
 }
 	"""
 
-    variables = {'id': listId}
+    variables = {"id": listId}
 
     url = "https://graphql.anilist.co"
 
     response = requests.post(url, json={"query": query, "variables": variables})
-    
+
     text_out = response.text
 
     with open("rel.json", "w") as out_file:
         out_file.write(text_out)
-    
+
+
+def runRel():
+    print("fuck")
+
+
 def parseRel():
     with open("rel.json") as json_data:
         data = json.load(json_data)
 
     parsed = data["data"]["Media"]["relations"]["nodes"]
-    
+
     parse_type = [x for x in parsed if x["type"] == "ANIME"]
-    
+
     i = 0
-    
-    while i < len(parse_type):
-        print(parsed[i]["id"])
-        i +=1
+
+    while i <= len(parse_type):
+        i = 1
 
     objects = json.dumps(parse_type, indent=4)
 
     with open("relations.json", "w") as out_file:
         out_file.write(objects)
 
-    os.remove("rel.json")    
+    os.remove("rel.json")
 
-relations()
-parseRel()
+
+def planFilter():
+    with open("planning.json") as jsonData:
+        planData = json.load(jsonData)
+
+    output = []
+
+    f = 0
+
+    for item in range(len(planData)):
+        print(planData[f])
+        print(planData[f]["media"]["id"])
+        output.append(planData[f]["media"]["id"])
+        f = 1
+
+    planObjects = json.dumps(output, indent=4)
+
+    with open("planning_filtered.json", "w") as out_file:
+        out_file.write(planObjects)
