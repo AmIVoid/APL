9fef0b4b4ad86f9ca2489d7f78f2eb3f0f2a2147
AmIVoid <fukinvoid@gmail.com>
2021-01-05 15:58:11 +1000

    Added import of p_planning to Google Sheets

--- APL.py
+++ APL.py
@@ -1,11 +1,14 @@
 from planning import search, searchParse
 from completed import completed_search, compParse
 from pFactor import runRel, idChecker, relFilter, filterList
+from sheets import sheets
 
 
 def APL():
 
     user = input("AniList username: ")
+    
+    sheetId = input("Google Sheets ID: ")
 
     search(user)
     searchParse()
@@ -16,6 +19,6 @@ def APL():
     runRel()
     relFilter()
     idChecker()
-
+    sheets(sheetId)
 
 APL()
